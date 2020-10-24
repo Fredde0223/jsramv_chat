1fcab31466b6257ecbe211305da775f16ae7c82d
Fredde0223 <4hnell@gmail.com>
2020-10-24 15:40:28 +0100

    kmom06

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+const mongo = require("mongodb").MongoClient;
+const dsn = "mongodb://localhost:27017/chatlog";
+
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
@@ -9,7 +21,42 @@ io.on('connection', function (socket) {
 
     socket.on('send message', function (body) {
         io.emit('message', body);
+
+        addToCollection(dsn, "msghistory", body.body);
     });
 });
 
+app.get("/", async (request, response) => {
+    try {
+        let res = await findInCollection(dsn, "msghistory", {}, {}, 0);
+
+        console.log(res);
+        response.json(res);
+    } catch (err) {
+        console.log(err);
+        response.json(err);
+    }
+});
+
 server.listen(3000);
+
+async function findInCollection(dsn, colName, criteria, projection, limit) {
+    const client  = await mongo.connect(dsn);
+    const db = await client.db();
+    const col = await db.collection(colName);
+    const res = await col.find(criteria, projection).limit(limit).toArray();
+
+    await client.close();
+
+    return res;
+}
+
+async function addToCollection(dsn, colName, message) {
+    const client  = await mongo.connect(dsn);
+    const db = await client.db();
+    const col = await db.collection(colName);
+
+    await col.insertOne( { msg: message } );
+
+    await client.close();
+}
